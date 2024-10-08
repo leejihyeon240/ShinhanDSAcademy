19010937ac7508f973ee38b39800846c6182bd82
leejihyeon240 <xnaxna1@naver.com>
2024-10-08 11:09:07 +0900

    2024.10.07 계좌 관리 프로그램

--- thisisjava/src/ch06/Account.java
+++ thisisjava/src/ch06/Account.java
@@ -2,17 +2,43 @@ package ch06;
 
 public class Account {
 	
-	String account;
-	String name;
-	int initMoney;
+	private String account;
+	private String name;
+	private int money;
 	
-	public Account(String account, String name, int initMoney) {
+	public Account(String account, String name, int money) {
 		this.account = account;
 		this.name = name;
-		this.initMoney = initMoney;
+		this.money = money;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMoney() {
+		return money;
+	}
+
+	public void setMoney(int money) {
+		this.money = money;
 	}
 	
-	void create() {
-		
+	@Override
+	public String toString() {
+	    return account + "   " + name + "   " + money;
 	}
+	
 }
