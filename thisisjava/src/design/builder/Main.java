92f8d41e4e1fa2bc34b622743d8e70805b0b7ef1
leejihyeon240 <xnaxna1@naver.com>
2024-10-28 16:13:31 +0900

    2024.10.28 oracle eclipse(java) DB 연결

--- thisisjava/src/design/builder/Main.java
+++ thisisjava/src/design/builder/Main.java
@@ -4,10 +4,20 @@ public class Main {
 
 	public static void main(String[] args) {
 		// 빌더 패턴을 사용한 클래스의 객체 생성
-		Member member = new Member.MemberBuilder().setId("hong").setPwd("111").setName("홍길동").build();
 
-		System.out.println("ID: " + member.getId());
-		System.out.println("Password: " + member.getPwd());
-		System.out.println("Name: " + member.getName());
+int score = 85;
+String grade = "";
+switch (score / 10) {
+    case 9 :
+        grade = "A";
+    case 8 :
+        grade = "B";
+    case 7 :
+        grade = "C";
+    default :
+        grade = "D";
+}
+System.out.println(grade);
+
 	}
 }
