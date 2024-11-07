13af41ea04bec9c7a02f7331b6b7bb9ec166fa9f
leejihyeon240 <xnaxna1@naver.com>
2024-11-07 17:10:53 +0900

    2024.11.07 웹 서비스 인프라 구축

--- js/script1.js
+++ js/script1.js
@@ -1,5 +1,10 @@
 let heading = document.querySelector('#heading');
 console.log(heading);
-heading.onclick = function () {
+// heading.onclick = function () {
+//     heading.style.color = "red";
+// };
+console.log(typeof heading);
+function test() {
     heading.style.color = "red";
-};
+}
+console.log(typeof test);
